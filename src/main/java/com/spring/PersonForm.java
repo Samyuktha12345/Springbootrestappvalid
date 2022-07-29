7040cfb3b61f305724c6a11ff896593a2f760817
Samyuktha12345 <98936911+Samyuktha12345@users.noreply.github.com>
2022-07-29 18:40:50 +0530

    Add files via upload
    first commit

---
+++ src/main/java/com/spring/PersonForm.java
@@ -0,0 +1,89 @@
+package com.spring;
+
+import java.util.Date;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class PersonForm {
+	@Size(min=2, max=30)
+	@NotEmpty(message = "Name may not be null")
+	private String name;
+	
+	@NotNull(message="Enter proper age")
+	@Min(18)
+	private Integer age;
+	
+	@Email(message = "Email is not valid", regexp =  "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+	private String email;
+	
+	@NotNull
+	@DateTimeFormat(pattern=("MM/dd/yyyy"))
+	private Date birthday;
+	
+	@Size(min=10)
+	@NotEmpty(message = "Mobileno not be null")
+   // @Pattern(regexp = "^[0-9]{1,3}\\\\.[0-9]{1,3}\\\\.[0-9]{1,3}\\\\.[0-9] {1,3}$")
+	private String mobileno;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+
+	public PersonForm(@Size(min = 2, max = 30) @NotEmpty(message = "Name may not be null") String name,
+			@NotNull(message = "Enter proper age") @Min(18) Integer age,
+			@Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") String email,
+			Date birthday, @Size(min = 10) @NotEmpty(message = "Mobileno not be null") String mobileno) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.birthday = birthday;
+		this.mobileno = mobileno;
+	}
+
+	
+	
+}
---
+++ src/main/java/com/spring/SpringBootRestAppValidApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAppValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestAppValidApplication.class, args);
+	}
+
+}
